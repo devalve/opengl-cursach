87c24b4f2281722a347e4764e13e30884d1e2d43
Devalve <sekiro.shad@gmail.com>
2021-04-23 09:18:19 +0400

    kursach done

--- more lights/Hello_Window/main.cpp
+++ more lights/Hello_Window/main.cpp
@@ -171,13 +171,61 @@ float second_cube_vertices[] = {
    -0.5f,  0.3f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
    -0.5f,  0.3f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
 };
+float third_cube_vertices[] = {
+    // координаты        // нормали           // текстурные координаты
+   -0.5f, -0.3f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+    0.5f, -0.3f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+    0.5f,  0.3f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+    0.5f,  0.3f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+   -0.5f,  0.3f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+   -0.5f, -0.3f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+   -0.5f, -0.3f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+    0.5f, -0.3f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+    0.5f,  0.3f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+    0.5f,  0.3f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+   -0.5f,  0.3f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+   -0.5f, -0.3f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+   -0.5f,  0.3f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+   -0.5f,  0.3f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+   -0.5f, -0.3f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+   -0.5f, -0.3f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+   -0.5f, -0.3f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+   -0.5f,  0.3f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+    0.5f,  0.3f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+    0.5f,  0.3f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+    0.5f, -0.3f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+    0.5f, -0.3f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+    0.5f, -0.3f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+    0.5f,  0.3f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+   -0.5f, -0.3f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+    0.5f, -0.3f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+    0.5f, -0.3f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+    0.5f, -0.3f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+   -0.5f, -0.3f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+   -0.5f, -0.3f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+   -0.5f,  0.3f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+    0.5f,  0.3f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+    0.5f,  0.3f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+    0.5f,  0.3f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+   -0.5f,  0.3f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+   -0.5f,  0.3f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+};
 // Координаты всех контейнеров
 glm::vec3 cubePositions[] = {
     glm::vec3(0.0f,  0.0f,  0.0f),
 
 };
 glm::vec3 second_cube_positions[] = {
-    glm::vec3(-0.8f,  0.0f,  -0.4f),
+    glm::vec3(-0.8f,  0.0f,  -0.5f),
+
+};
+glm::vec3 third_cube_positions[] = {
+    glm::vec3(-0.8f,  0.0f,  -0.8f),
 
 };
 // Координаты точечных источников света
@@ -230,14 +278,14 @@ int main()
  
 
     // 1. Настраиваем VAO (и VBO) куба
-    unsigned int VBOs[3], cubeVAO[2];
-    glGenVertexArrays(2, cubeVAO);
+    unsigned int VBOs[3], cubeVAOs[3];
+    glGenVertexArrays(3, cubeVAOs);
     glGenBuffers(2, VBOs);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindVertexArray(cubeVAO[0]);
+    glBindVertexArray(cubeVAOs[0]);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -248,7 +296,18 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(second_cube_vertices), second_cube_vertices, GL_STATIC_DRAW);
 
-    glBindVertexArray(cubeVAO[1]);
+    glBindVertexArray(cubeVAOs[1]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(third_cube_vertices), third_cube_vertices, GL_STATIC_DRAW);
+
+    glBindVertexArray(cubeVAOs[2]);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -371,7 +430,7 @@ int main()
         glBindTexture(GL_TEXTURE_2D, specularMap);
 
         // Рендеринг контейнеров
-        glBindVertexArray(cubeVAO[0]);
+        glBindVertexArray(cubeVAOs[0]);
         for (unsigned int i = 0; i < 1; i++)
         {
             // Вычисляем матрицу модели для каждого объекта и передаем её в шейдер
@@ -383,19 +442,26 @@ int main()
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
         } 
-        glBindVertexArray(cubeVAO[1]);
-        for (unsigned int i = 0; i < 1; i++)
-        {
+        glBindVertexArray(cubeVAOs[1]);
+       
             // Вычисляем матрицу модели для каждого объекта и передаем её в шейдер
-            glm::mat4 model = glm::mat4(1.0f);
+             model = glm::mat4(1.0f);
             model = glm::mat4(1.0f);
-            model = glm::translate(model, second_cube_positions[i]);
-            float angle = 20.0f*i;
+            model = glm::translate(model, second_cube_positions[0]);
+            float angle = 20.0f*0;
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             model = glm::scale(model, glm::vec3(0.6f)); // меньший куб
             lightingShader.setMat4("model", model);
             glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+
+            glBindVertexArray(cubeVAOs[2]);
+            model = glm::mat4(1.0f);
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, third_cube_positions[0]);
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::scale(model, glm::vec3(1.0f,1.0f,0.2f)); // меньший куб
+            lightingShader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
 
        
         glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -421,9 +487,9 @@ int main()
     }
 
     // Опционально: освобождаем все ресурсы, как только они выполнили свое предназначение
-    glDeleteVertexArrays(2, cubeVAO);
+    glDeleteVertexArrays(3, cubeVAOs);
     glDeleteVertexArrays(1, &lightCubeVAO);
-    glDeleteBuffers(1, VBOs);
+    glDeleteBuffers(3, VBOs);
 
     // glfw: завершение, освобождение всех выделенных ранее GLFW-реcурсов
     glfwTerminate();
